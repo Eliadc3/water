f5b4a54708b5b240760f4aa5179f78f212085d05
Eliad Cohen <eliadc32302@gmail.com>
2023-05-27 00:51:44 +0300

    update

--- client/src/components/graph.js
+++ client/src/components/graph.js
@@ -6,52 +6,64 @@ const Graph = () => {
   const [chartOptions, setChartOptions] = useState({});
   const [series, setSeries] = useState([]);
 
-  //const [data, setData] = useState(null);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         // Perform the data fetching here
-        const response = fetch("manipulations");
-        const jsonData = response.json();
-        // Process the jsonData and extract the necessary data for the chart
-        const chartData = jsonData.map((item) => ({
-          x: item.Time,
-          y: item.Stage1_concentrate_flow_m3h,
-        }));
-
-        // Update the chart options and series state variables
-        setChartOptions({
-          chart: {
-            type: "line",
-          },
-          xaxis: {
-            categories: chartData.map((item) => item.x),
-          },
-          // Add any additional chart options here
-        });
-        setSeries([
-          {
-            name: "Concentrate Flow",
-            data: chartData.map((item) => item.y),
-          },
-        ]);
-        const chart = new ApexCharts(
-          document.querySelector("#chart"),
-          chartOptions
+        const response = await fetch(
+          "http://localhost:5000/water/manipulations"
         );
-        chart.render();
+        console.log("response: " + response);
+        if (response.ok) {
+          const jsonData = await response.json();
+
+          // console.log(jsonData);
+
+          // // Process the jsonData and extract the necessary data for the chart
+          const chartData = jsonData.map((item) => ({
+            x: item.Time,
+            y: item.Stage1_concentrate_flow_m3h,
+          }));
+
+          // Update the chart options and series state variables
+          setChartOptions({
+            chart: {
+              type: "line",
+            },
+            xaxis: {
+              categories: chartData.map((item) => item.x),
+            },
+            // Add any additional chart options here
+          });
+          setSeries([
+            {
+              name: "Concentrate Flow",
+              data: chartData.map((item) => item.y),
+            },
+          ]);
+        } else {
+          throw new Error("Request failed with status: " + response.status);
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, [chartOptions]);
+  }, []);
 
+  useEffect(() => {
+    const chart = new ApexCharts(
+      document.querySelector("#chart"),
+      chartOptions
+    );
+    chart.render();
+  }, [chartOptions]);
   // Rest of the component code when data is available
   return (
     <div>
+      <script src="https://cdn.jsdelivr.net/npm/apexcharts@latest"></script>
+      <div id="chart"></div>
       <ReactApexChart
         options={chartOptions}
         series={series}
--- server/routes/water_router.js
+++ server/routes/water_router.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+const URI = process.env.URI;
+
 const manipulationsController = require("../controllers/manipulations_controller.js");
 
 const multer = require("multer");
@@ -9,5 +13,26 @@ router.post(
   uploadPre.single("file"),
   manipulationsController.manipulations
 );
+
+router.get("/manipulations", (req, res) => {
+  MongoClient.connect(URI, (err, client) => {
+    if (err) {
+      console.error("Error connecting to MongoDB:", err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    const collection = client.db("test").collection("current_datas");
+    collection.find({}).toArray((err, data) => {
+      if (err) {
+        console.error("Error retrieving data from database:", err);
+        res.status(500).json({ error: "Internal server error" });
+      } else {
+        console.log(data);
+        res.json(data);
+      }
+      client.close();
+    });
+  });
+});
 
 module.exports = router;
