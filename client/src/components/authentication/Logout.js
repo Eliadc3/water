1566a246e4793fa38ef9a6bff09930c29eb04f86
Eliad Cohen <eliadc32302@gmail.com>
2023-08-25 17:10:22 +0300

    Fixed directory capitalization

---
+++ client/src/components/authentication/Logout.js
@@ -0,0 +1,25 @@
+import Cookies from "js-cookie";
+import React from "react";
+
+const Logout = () => {
+  const handleLogout = () => {
+    // Remove token from Cookies
+    Cookies.remove("token");
+
+    // Clear user-related data if necessary
+    Cookies.remove("user");
+
+    // Remove role(admin) from Cookies
+    Cookies.remove("admin");
+
+    // Remove username from localStorage
+    Cookies.remove("username");
+
+    // Redirect the user to the login page
+    window.location.href = "/login";
+  };
+
+  return <span onClick={handleLogout}>Logout</span>;
+};
+
+export default Logout;
